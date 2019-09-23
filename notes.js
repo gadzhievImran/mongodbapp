5f5d6edadf3166a449ee830c996509f01a51f136
Ima Gadzhiev <sdamegeoge@gmail.com>
2019-09-24 01:02:47 +0300

    update

--- notes.js
+++ notes.js
@@ -12,7 +12,20 @@ const client = new MongoClient('mongodb://localhost:27017', { useNewUrlParser: t
     const collection = db.collection('notes');
     
     // create
-    const result = await collection.insertOne({ title: 'Hello World' });
-    console.log(result, 'result');
+    // const result = await collection.insertOne({ title: 'Hello World' });
+    // console.log(result, 'result');
+
+    // read
+    // const notes = await collection.find().toArray();
+    // console.log(notes, 'notes');
+    
+    // read one
+    // const note = await collection.findOne();
+    // console.log(note, 'note');
+    
+    // update
+    // 15:23
+    
+    client.close();
   }
 )();
