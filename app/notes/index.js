084694065a898b4bbe948bb0bfea84a247804222
Ima Gadzhiev <sdamegeoge@gmail.com>
2019-09-29 18:17:30 +0300

    update

--- app/notes/index.js
+++ app/notes/index.js
@@ -10,8 +10,23 @@ router.get('/', async (req,res) => {
   const notes = await collection.find().toArray();
   
   res.render('notes/views/index', {
-    notes 
+    notes
   });
 });
+
+router.get('/create', (req,res) => {
+  res.render('notes/views/create');
+});
+
+router.post('/create', async (req,res) => {
+  const note = req.body;
+  const client = req.app.client;
+  const db = client.db('express-notes');
+  const collection = db.collection('notes');
+  
+  await collection.insertOne(note);
+  res.redirect('/');
+});
+
 
 module.exports = router;
